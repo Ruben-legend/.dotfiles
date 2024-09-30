4d1d288e74f0db6c6559014dc79f886870ce43ba
ruben-legend <rubenalexander644@outlook.es>
2024-09-30 10:05:14 -0400

    rebuild: workspace set up

--- wm/hyprland/configs/workspace.nix
+++ wm/hyprland/configs/workspace.nix
@@ -1,15 +1,16 @@
-{config, monitors, ...}:{
+{config, laptop, monitors, ...}:{
 
  wayland.windowManager.hyprland = {
 
    settings = {   
-      workspace = [
+      workspace = [] ++ (if laptop then
+        [
+          "1, monitor:${monitors.laptop}"
+        ] 
+      else[
         "1, monitor:${monitors.pc.m1}"
-        "2, monitor:${monitors.pc.m1}"
-        "3, monitor:${monitors.pc.m1}"
-        "4, monitor:${monitors.pc.m1}"
         "9, monitor:${monitors.pc.m2}"
-      ];
+        ]) ;
     };
   };
 }
