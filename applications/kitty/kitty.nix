dceecc9c93a1c17be8613cee5291abab2eb37ed7
ruben-legend <rubenalexander644@outlook.es>
2024-10-01 08:36:36 -0400

    add: transparent background on kitty

--- applications/kitty/kitty.nix
+++ applications/kitty/kitty.nix
@@ -2,11 +2,19 @@
   programs.kitty = {
     enable = true;
     
-    settings = {
-      background_opacity = "0.5";
-      #background_blur = 1;
-      map = "f1 launch";
-    };
+    extraConfig = "
+      background_opacity 0.5
+      
+      map alt+n new_tab 
+      map alt+q close_tab
+      map alt+[ next_tab
+      map alt+] previous_tab
+
+      map alt+w new_window
+      map alt+e close_window
+      map alt+l next_window
+      map alt+h previous_window
+    ";
 
   };
 }
